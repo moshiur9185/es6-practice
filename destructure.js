cfd4ccb74f19f97b73b2332450432c4ab8696aef
moshiur9185 <moshiur9185@gmail.com>
2021-02-02 09:14:53 +0600

    work done

---
+++ destructure.js
@@ -0,0 +1,16 @@
+const person = { name: 'jack', age: 17, phone: 01722432465, job: 'anyJob', gfName: 'Watson' }
+const { name, phone } = person;
+
+// const Name = person.name;
+// const phone = person.phone;
+// console.log(name, phone);
+
+complexObject={
+    name : 'Abrar',
+    info:{
+        address : "kolatola",
+        leader :'banor'
+    }
+
+}
+const {leader} = complexObject.info;
